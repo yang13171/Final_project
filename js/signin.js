dfff382907d1c29dce71eacb446a342b3ba74d72
annieli-py <52976777+annieli-py@users.noreply.github.com>
2019-08-02 11:13:19 -0700

    Add files via upload

---
+++ js/signin.js
@@ -0,0 +1,21 @@
+var counter = 0;
+
+function check() {
+  var name = "",
+    password = "";
+
+  name = form1.txtusername.value
+  password = form1.txtpassword.value
+
+}
+
+  if () {
+  window.location.replace("pickupinfo.html");
+  form1.txtuser.value = ''
+  form1.txtpassword.value = ''
+
+}
+
+  else if () {
+  window.alert("Incorrect password. Try again.")
+}
